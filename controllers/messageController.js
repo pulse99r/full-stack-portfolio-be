d691d33e5ae70dbfc3dd23da81638bdc926ae9db
Raymond <raymondjohnson@pursuit.org>
2023-08-21 01:58:28 -0400

    create message in progress; partially working!

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -38,15 +38,28 @@ messages.get("/:id", async (req, res) => {
 });
 
 
-// CREATE New Message
-messages.get("/:id", async (req, res) => {
-  console.log("--- getting single message id=1 ---")
-  const id = req.params.id;
-  const oneMessage = await getMessage(id);
-  if (oneMessage) {
-    res.status(200).json(oneMessage);
+/* CREATE New Message    */
+/* Pick Recipient User */
+
+
+ /* Validate message */
+const validateMessage = (req, res, next) => {
+  const { topic, msg_body, sender_id, recipient_id, reply_to } = req.body;
+  if (req.body.topic && req.body.msg_body) {
+    console.log("Validated - Creating message record")
+    next();
   } else {
-    res.status(500).json({ error: "server error" });
+    res.status(400).json({ error: "Message cannot be left empty!" });
+  }
+};
+
+// ...then CREATE NEW MESSAGE
+messages.post("/", validateMessage, async (req, res) => {
+  try {
+    const theNewMessage = await createMessage(req.body);
+    res.status(200).json(theNewMessage);
+  } catch (error) {
+    res.status(400).json({ error: "Something went wrong" });
   }
 });
 
